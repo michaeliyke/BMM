f36e8fd8a30e2430a308904ad2c7d5f31406b041
Michael Iyke <ymichaelc@gmail.com>
2024-05-23 16:55:17 +0100

    Directory resturing

--- tables.sql
+++ tables.sql
@@ -1,35 +1,81 @@
--- The Bookmark tableCREATE TABLE Bookmarks (
-bookmark_id INT PRIMARY KEY AUTO_INCREMENT,
-title VARCHAR(255) NOT NULL,
-url VARCHAR(2048),
-description TEXT,
--- Other bookmark specific fields
-CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES Users(user_id)
+-- Drop database
+DROP DATABASE IF EXISTS BMM_db;
+-- Create the BMM database
+CREATE DATABASE IF NOT EXISTS BMM_db;
+-- Create tables
+USE BMM_db;
+-- Drop table
+DROP TABLE IF EXISTS users;
+-- The users table
+CREATE TABLE IF NOT EXISTS users (
+	`id` VARCHAR(60) NOT NULL PRIMARY KEY,
+	`username` VARCHAR(255) DEFAULT NULL,
+	`password` VARCHAR(255) DEFAULT NULL,
+	`email` VARCHAR(255) DEFAULT NULL,
+	`first_name` VARCHAR(255) DEFAULT NULL,
+	`last_name` VARCHAR(255) DEFAULT NULL,
+	`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
--- The tags table
-CREATE TABLE Tags (
-	tag_id INT PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(128) NOT NULL UNIQUE
+-- Drop table
+DROP TABLE IF EXISTS bookmarks;
+-- The bookmarks table
+CREATE TABLE IF NOT EXISTS bookmarks (
+	`id` VARCHAR(60) NOT NULL PRIMARY KEY,
+	`title` VARCHAR(255) DEFAULT NULL,
+	`url` VARCHAR(1024) NOT NULL,
+	`description` VARCHAR(1024) DEFAULT NULL,
+	`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+-- Drop table
+DROP TABLE IF EXISTS categories;
 -- The categories table
-CREATE TABLE Categories (
-	category_id INT PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(128) NOT NULL UNIQUE
+CREATE TABLE IF NOT EXISTS categories (
+	`id` VARCHAR(60) NOT NULL PRIMARY KEY,
+	`name` VARCHAR(255) NOT NULL,
+	`user_id` VARCHAR(60) NOT NULL,
+	`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
 );
--- The Bookkmark_tags junction table
-CREATE TABLE BookmarksTags (
-	bookmark_id INT NOT NULL,
-	tag_id INT NOT NULL,
-	PRIMARY KEY (bookmark_id, tag_id),
-	CONSTRAINT fk_bookmark FOREIGN KEY (bookmark_id) REFERENCES Bookmarks(bookmark_id),
-	CONSTRAINT fk_tag FOREIGN KEY (tag_id) REFERENCES Tags(tag_id)
+-- Drop table
+DROP TABLE IF EXISTS tags;
+-- The tags table
+CREATE TABLE IF NOT EXISTS tags (
+	`id` VARCHAR(60) NOT NULL PRIMARY KEY,
+	`name` VARCHAR(255) NOT NULL,
+	`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
--- The Bookmarks_categories junction table
-CREATE TABLE BookmarksCategories (
-	bookmark_id INT NOT NULL,
-	category_id INT NOT NULL,
+-- Drop table
+DROP TABLE IF EXISTS bookmark_category;
+-- The bookmark_category junction table
+CREATE TABLE IF NOT EXISTS bookmark_category (
+	bookmark_id VARCHAR(60) NOT NULL,
+	category_id VARCHAR(60) NOT NULL,
 	PRIMARY KEY (bookmark_id, category_id),
-	CONSTRAINT fk_bookmark_cat FOREIGN KEY (bookmark_id) REFERENCES Bookmarks(bookmark_id),
-	CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES Categories(category_id)
+	FOREIGN KEY (bookmark_id) REFERENCES bookmarks(id),
+	FOREIGN KEY (category_id) REFERENCES categories(id)
 );
-- -
+-- Drop table
+DROP TABLE IF EXISTS bookmark_tag;
+-- The bookmark_tag junction table
+CREATE TABLE IF NOT EXISTS bookmark_tag (
+	bookmark_id VARCHAR(60) NOT NULL,
+	tag_id VARCHAR(60) NOT NULL,
+	PRIMARY KEY (bookmark_id, tag_id),
+	FOREIGN KEY (bookmark_id) REFERENCES bookmarks(id),
+	FOREIGN KEY (tag_id) REFERENCES tags(id)
+);
+-- Create a db user for the application to use
+CREATE USER IF NOT EXISTS 'BMM_db_user' @'localhost';
+SET PASSWORD FOR 'BMM_db_user' @'localhost' = 'BMM_db_user_password';
+-- Grant all privileges on the example_db database to the test user
+GRANT SELECT,
+	INSERT,
+	UPDATE,
+	DELETE ON BMM_db.* TO 'BMM_db_user' @'localhost';
+GRANT SELECT ON performance_schema.* to 'BMM_db_user' @'localhost';
+-- Flush privileges to ensure that they are reloaded by the server
+FLUSH PRIVILEGES;
