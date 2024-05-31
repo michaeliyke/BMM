c9b83c12603df038ce65600a5f96c46491753bed
Michael Iyke <ymichaelc@gmail.com>
2024-05-31 16:31:10 +0100

    Modified landing page texts
    Cleanup progress on going live

--- notes.sh
+++ notes.sh
@@ -4,8 +4,8 @@
 # ExecStart=/home/michaeliyke/michaeliyke/projects/BMM/bin/gunicorn --bind unix:$PROJ_ROOT/bmm.sock wsgi:app -m 777 --workers 4 --threads 2 --timeout 90 --log-level=debug --log-file="$PROJ_ROOT/logs/gunicorn.log" --access-logfile="$PROJ_ROOT/logs/access.log" --error-logfile="$PROJ_ROOT/logs/error.log" --capture-output --enable-stdio-inheritance
 
 # /etc/systemd/system/bmm.service
-# sudo systemctl start bmm
 # sudo systemctl enable bmm
+# sudo systemctl start bmm
 
 # gunicorn --bind 0.0.0.0:5000 wsgi:app --workers 4 --threads 2 --timeout 90 --log-level=debug --log-file="$PROJ_ROOT/logs/gunicorn.log" --access-logfile="$PROJ_ROOT/logs/access.log" --error-logfile="$PROJ_ROOT/logs/error.log" --capture-output --enable-stdio-inheritance
 # ExecStart=/home/michaeliyke/michaeliyke/projects/BMM/bin/gunicorn --bind unix:bmm.sock wsgi:app -m 007 --workers 2 --threads 2 --timeout 90 --log-level=debug --log-file=logs/gunicorn.log --access-logfile=logs/access.log --error-logfile=logs/error.log --capture-output --enable-stdio-inheritance
