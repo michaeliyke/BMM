79cfdf07e67ce9c305e5c867eb1ce21c39a2dd43
Michael Iyke <ymichaelc@gmail.com>
2024-06-06 14:35:03 +0100

    Quick patch

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This module instantiates an object of class a named storage engine"""
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 storage_type = os.getenv("STORAGE_TYPE")
 if not storage_type or storage_type == "db":
