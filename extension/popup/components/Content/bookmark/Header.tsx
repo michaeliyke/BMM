5aa91deae3d1cae88a1856dd40e465b2c4568035
Michael C. Iyke <ymichaelc@gmail.com>
2025-07-01 11:00:26 +0100

    prop drilling

--- extension/popup/components/Content/bookmark/Header.tsx
+++ extension/popup/components/Content/bookmark/Header.tsx
@@ -4,34 +4,37 @@ import { AiOutlineEdit } from "react-icons/ai";
 import { BsTrash } from "react-icons/bs";
 import { FaClock, FaRegStar } from "react-icons/fa";
 import { MdOutlineArchive } from "react-icons/md";
-import { IBookmark } from "../../../utils/types/schemas";
+import { useAppState } from "../../../hooks/globalstate";
 import Archive from "../archived/Archive";
 import Delete from "../deleted/Delete";
 
 type HeaderProps = {
-  bookmark: IBookmark;
   showDetails: boolean;
   setShowDetails: Dispatch<SetStateAction<boolean>>;
   initiateEditing: () => void;
 };
 
 export default function Header(props: HeaderProps) {
-  const { bookmark, showDetails, setShowDetails, initiateEditing } = props;
+  const { showDetails, setShowDetails, initiateEditing } = props;
 
   const [deleteDialog, setDeleteDialog] = useState(false);
   const [archiveDialog, setArchiveDialog] = useState(false);
+  const { bookmarkToShow } = useAppState();
+
+  // Never runs since this it the Header details of a selected bookmark.
+  if (!bookmarkToShow) return null;
 
 
   return <header className="mb-4 flex flex-col">
 
     {/* The url detail */}
     <a
-      href={bookmark.url}
+      href={bookmarkToShow.url}
       target="_blank"
       rel="noopener noreferrer"
       className="text-blue-900 py-2 bg-white hover:underline text-sm w-full text-center"
     >
-      {bookmark.url.length > 65 ? `${bookmark.url.substring(0, 65)}...` : bookmark.url}
+      {bookmarkToShow.url.length > 65 ? `${bookmarkToShow.url.substring(0, 65)}...` : bookmarkToShow.url}
     </a>
 
 
@@ -111,19 +114,19 @@ export default function Header(props: HeaderProps) {
 
 
         {/* The time widget */}
-        <time dateTime={bookmark.updated_at} className="flex items-center text-xs/2 space-x-1">
+        <time dateTime={bookmarkToShow.updated_at} className="flex items-center text-xs/2 space-x-1">
           <FaClock className="text-gray-400" aria-hidden="true" />
-          <span>{moment(bookmark.updated_at).fromNow()}</span>
+          <span>{moment(bookmarkToShow.updated_at).fromNow()}</span>
         </time>
       </aside>
     </div>
     <Delete
-      bookmark={bookmark}
+      bookmark={bookmarkToShow}
       dialog={deleteDialog}
       setDialog={setDeleteDialog}
     />
     <Archive
-      bookmark={bookmark}
+      bookmark={bookmarkToShow}
       dialog={archiveDialog}
       setDialog={setArchiveDialog}
     />
